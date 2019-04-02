7c3d544c26a25fa5e7c5866254f6157a1fdcd234
CRIMX <straybugs@gmail.com>
2019-04-02 19:44:19 +0800

    add eslint react hooks plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,10 @@ module.exports = {
     node: true
   },
   extends: ['standard', 'standard-react'],
+  plugins: ['react-hooks'],
   rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
