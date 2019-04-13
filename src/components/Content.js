6c7ab5509ea8163a1a681bd2b3986c58a10805fb
CRIMX <straybugs@gmail.com>
2019-04-13 08:06:09 +0800

    fix: remove style

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,24 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export const HTMLContent = ({ content, className, style }) => (
-  <div
-    className={className}
-    style={style}
-    dangerouslySetInnerHTML={{ __html: content }}
-  />
+export const HTMLContent = ({ content, className }) => (
+  <div className={className} dangerouslySetInnerHTML={{ __html: content }} />
 )
 
-const Content = ({ content, className, style }) => (
-  <div className={className} style={style}>
-    {content}
-  </div>
+const Content = ({ content, className }) => (
+  <div className={className}>{content}</div>
 )
 
 Content.propTypes = {
   content: PropTypes.node,
-  className: PropTypes.string,
-  style: PropTypes.style
+  className: PropTypes.string
 }
 
 HTMLContent.propTypes = Content.propTypes
