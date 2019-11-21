8fd70f0e8ba606660d62a2bb1f0dd0de5d788010
crimx <straybugs@gmail.com>
2019-11-21 09:50:35 +0800

    refactor: remove doubanshow

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,100 +5,99 @@ import Img from 'gatsby-image'
 import Layout from '../components/Layout'
 import Navbar from '../components/Navbar'
 import Quote from '../components/Quote'
-import DoubanShow from '../components/DoubanShow'
 import logo from '../img/logo.svg'
 
 const IndexPage = ({
-  data: { heroimg, tagJavaScript, tagSource, tagSpec, tagTotal, latestPosts }
+  data: { heroimg, tagJavaScript, tagSource, tagSpec, tagTotal, latestPosts },
 }) => {
   return (
     <Layout>
-      <section className='hero has-gatsby-img'>
+      <section className="hero has-gatsby-img">
         <Img fluid={heroimg.childImageSharp.fluid} />
-        <div className='hero-head'>
+        <div className="hero-head">
           <Navbar />
         </div>
 
-        <div className='hero-body has-text-centered'>
-          <div className='container'>
+        <div className="hero-body has-text-centered">
+          <div className="container">
             <img
               style={{ width: '55vmin', height: '55vmin' }}
               src={logo}
-              alt='CRIMX LOGO'
+              alt="CRIMX LOGO"
             />
           </div>
         </div>
 
-        <div className='hero-foot has-text-centered'>
-          <svg viewBox='0 0 32 32' width='32' height='32'>
+        <div className="hero-foot has-text-centered">
+          <svg viewBox="0 0 32 32" width="32" height="32">
             <title>scroll down</title>
             <path
-              fill='#fff'
-              d='M.045 8.443c0-.215.082-.43.246-.594.33-.33.86-.33 1.19 0L16 22.37 30.52 7.85c.33-.33.86-.33 1.19 0s.327.86 0 1.186L16.593 24.152c-.328.326-.86.326-1.188 0L.29 9.036c-.163-.163-.245-.378-.245-.593z'
+              fill="#fff"
+              d="M.045 8.443c0-.215.082-.43.246-.594.33-.33.86-.33 1.19 0L16 22.37 30.52 7.85c.33-.33.86-.33 1.19 0s.327.86 0 1.186L16.593 24.152c-.328.326-.86.326-1.188 0L.29 9.036c-.163-.163-.245-.378-.245-.593z"
             />
           </svg>
         </div>
       </section>
 
-      <section className='section'>
-        <div className='container'>
+      <section className="section">
+        <div className="container">
           <Quote
             quote={{
               content: `"How do you know I'm mad?" said Alice.\n"You must be," said the Cat, "or you wouldn't have come here."`,
               author: `Lewis Carroll`,
-              source: `Alice's Adventures in Wonderland`
+              source: `Alice's Adventures in Wonderland`,
             }}
           />
         </div>
       </section>
 
-      <section className='section'>
-        <div className='container has-text-centered'>
-          <div className='content'>
+      <section className="section">
+        <div className="container has-text-centered">
+          <div className="content">
             <p>
               欢迎光临本博客！这里主要是记录 Web
               前端相关的一些文章，偶尔涉及其它我感兴趣的东西。
             </p>
           </div>
-          <div className='columns'>
-            <div className='column'>
+          <div className="columns">
+            <div className="column">
               <Link
-                className='has-text-dark is-block'
-                to='/archives?search=%23JavaScript'
+                className="has-text-dark is-block"
+                to="/archives?search=%23JavaScript"
               >
-                <div className='box has-background-light'>
-                  <h2 className='heading'>{tagJavaScript.totalCount} 篇文章</h2>
-                  <h1 className='title'>JAVASCRIPT</h1>
+                <div className="box has-background-light">
+                  <h2 className="heading">{tagJavaScript.totalCount} 篇文章</h2>
+                  <h1 className="title">JAVASCRIPT</h1>
                 </div>
               </Link>
             </div>
-            <div className='column'>
+            <div className="column">
               <Link
-                className='has-text-dark is-block'
-                to='/archives?search=%23闲读源码'
+                className="has-text-dark is-block"
+                to="/archives?search=%23闲读源码"
               >
-                <div className='box has-background-light'>
-                  <h2 className='heading'>{tagSource.totalCount} 篇文章</h2>
-                  <h1 className='title'>闲读源码</h1>
+                <div className="box has-background-light">
+                  <h2 className="heading">{tagSource.totalCount} 篇文章</h2>
+                  <h1 className="title">闲读源码</h1>
                 </div>
               </Link>
             </div>
-            <div className='column'>
+            <div className="column">
               <Link
-                className='has-text-dark is-block'
-                to='/archives?search=%23闲读规范'
+                className="has-text-dark is-block"
+                to="/archives?search=%23闲读规范"
               >
-                <div className='box has-background-light'>
-                  <h2 className='heading'>{tagSpec.totalCount} 篇文章</h2>
-                  <h1 className='title'>闲读规范</h1>
+                <div className="box has-background-light">
+                  <h2 className="heading">{tagSpec.totalCount} 篇文章</h2>
+                  <h1 className="title">闲读规范</h1>
                 </div>
               </Link>
             </div>
-            <div className='column'>
-              <Link className='has-text-dark is-block' to='/archives'>
-                <div className='box has-background-light'>
-                  <h2 className='heading'>{tagTotal.totalCount} 篇文章</h2>
-                  <h1 className='title'>全部博文</h1>
+            <div className="column">
+              <Link className="has-text-dark is-block" to="/archives">
+                <div className="box has-background-light">
+                  <h2 className="heading">{tagTotal.totalCount} 篇文章</h2>
+                  <h1 className="title">全部博文</h1>
                 </div>
               </Link>
             </div>
@@ -106,26 +105,26 @@ const IndexPage = ({
         </div>
       </section>
 
-      <section className='section'>
-        <div className='container'>
+      <section className="section">
+        <div className="container">
           <Quote
             quote={{
               content: `"Either write something worth reading or do something worth writing."`,
               author: `Benjamin Franklin`,
-              source: ``
+              source: ``,
             }}
           />
         </div>
       </section>
 
-      <section className='section'>
-        <div className='container'>
-          <div className='content'>
-            <p className='has-text-centered'>最近文章：</p>
+      <section className="section">
+        <div className="container">
+          <div className="content">
+            <p className="has-text-centered">最近文章：</p>
             {latestPosts.edges.map(({ node: post }) => (
-              <div className='box' key={post.id}>
+              <div className="box" key={post.id}>
                 <p>
-                  <Link className='is-link-reverse' to={post.fields.slug}>
+                  <Link className="is-link-reverse" to={post.fields.slug}>
                     <strong>{post.frontmatter.title}</strong>
                   </Link>
                   <span> &bull; </span>
@@ -133,7 +132,7 @@ const IndexPage = ({
                 </p>
                 <p>{post.frontmatter.description || post.excerpt}</p>
                 <p>
-                  <Link className='button is-small' to={post.fields.slug}>
+                  <Link className="button is-small" to={post.fields.slug}>
                     继续阅读 →
                   </Link>
                 </p>
@@ -141,17 +140,12 @@ const IndexPage = ({
             ))}
           </div>
           <Link
-            className='button has-text-weight-light is-medium is-light is-fullwidth'
-            to='/archives'
+            className="button has-text-weight-light is-medium is-light is-fullwidth"
+            to="/archives"
           >
             查看全部文章
           </Link>
         </div>
-      </section>
-
-      <section className='section content has-text-centered is-marginless-mobile'>
-        <p className='has-text-centered'>最近个人推荐的一些书和影视作品：</p>
-        <DoubanShow />
       </section>
     </Layout>
   )
@@ -160,24 +154,24 @@ const IndexPage = ({
 IndexPage.propTypes = {
   data: PropTypes.shape({
     heroimg: PropTypes.shape({
-      fluid: PropTypes.any
+      fluid: PropTypes.any,
     }),
     tagJavaScript: PropTypes.shape({
-      totalCount: PropTypes.number
+      totalCount: PropTypes.number,
     }),
     tagSource: PropTypes.shape({
-      totalCount: PropTypes.number
+      totalCount: PropTypes.number,
     }),
     tagSpec: PropTypes.shape({
-      totalCount: PropTypes.number
+      totalCount: PropTypes.number,
     }),
     tagTotal: PropTypes.shape({
-      totalCount: PropTypes.number
+      totalCount: PropTypes.number,
     }),
     latestPosts: PropTypes.shape({
-      edges: PropTypes.array
-    })
-  })
+      edges: PropTypes.array,
+    }),
+  }),
 }
 
 export default IndexPage
--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -5,19 +5,18 @@ import Layout from '../components/Layout'
 import Navbar from '../components/Navbar'
 import Utterances from '../components/Utterances'
 import Content, { HTMLContent } from '../components/Content'
-import DoubanShow from '../components/DoubanShow'
 
 // For preview
 export const AboutPageTemplate = ({ content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
   return (
-    <section className='section is-marginless-top-mobile'>
-      <div className='container'>
-        <div className='columns'>
-          <div className='column is-10-desktop is-offset-1'>
-            <section className='postify'>
-              <PageContent className='content' content={content} />
+    <section className="section is-marginless-top-mobile">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-10-desktop is-offset-1">
+            <section className="postify">
+              <PageContent className="content" content={content} />
             </section>
           </div>
         </div>
@@ -28,7 +27,7 @@ export const AboutPageTemplate = ({ content, contentComponent }) => {
 
 AboutPageTemplate.propTypes = {
   content: PropTypes.node,
-  contentComponent: PropTypes.func
+  contentComponent: PropTypes.func,
 }
 
 const AboutPage = ({ data }) => {
@@ -40,17 +39,13 @@ const AboutPage = ({ data }) => {
     >
       <Navbar />
       <AboutPageTemplate content={post.html} contentComponent={HTMLContent} />
-      <section className='section content has-text-centered is-marginless-mobile'>
-        <p className='has-text-centered'>最近个人推荐的一些书和影视作品：</p>
-        <DoubanShow />
-      </section>
-      <Utterances slug='about' />
+      <Utterances slug="about" />
     </Layout>
   )
 }
 
 AboutPage.propTypes = {
-  data: PropTypes.object.isRequired
+  data: PropTypes.object.isRequired,
 }
 
 export default AboutPage
