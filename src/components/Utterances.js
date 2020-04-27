59647d49b50b1fb5fcaad8f9f7521f74fa5c7b0a
crimx <straybugs@gmail.com>
2020-04-27 17:39:14 +0800

    refactor: add comment placeholder

--- src/components/Utterances.js
+++ src/components/Utterances.js
@@ -1,21 +1,26 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 
 const Utterances = React.memo(({ slug }) => {
+  const [loaded, setLoaded] = useState(false)
   const utterancesRef = useRef()
   useEffect(() => {
     if (typeof window === 'undefined') {
       return
     }
+    setLoaded(false)
     const el = document.createElement('script')
-    el.src = 'https://utteranc.es/client.js'
-    el.async = true
-    el.setAttribute('repo', 'crimx/blog-comments')
-    el.setAttribute('issue-term', slug)
-    el.setAttribute('label', 'Comment')
-    el.setAttribute('theme', 'github-light')
-    el.setAttribute('crossorigin', 'anonymous')
     if (utterancesRef.current) {
+      el.src = 'https://utteranc.es/client.js'
+      el.async = true
+      el.setAttribute('repo', 'crimx/blog-comments')
+      el.setAttribute('issue-term', slug)
+      el.setAttribute('label', 'Comment')
+      el.setAttribute('theme', 'github-light')
+      el.setAttribute('crossorigin', 'anonymous')
+      el.onload = () => {
+        setLoaded(true)
+      }
       utterancesRef.current.appendChild(el)
     }
     return () => {
@@ -23,7 +28,20 @@ const Utterances = React.memo(({ slug }) => {
     }
   }, [slug])
 
-  return <section key={slug} className='section' ref={utterancesRef} />
+  return (
+    <section key={slug} className='section' ref={utterancesRef}>
+      {loaded || (
+        <div className='has-text-centered'>
+          <div className='box is-inline-block'>
+            <p className='is-size-6'>评论没有加载，检查你的局域网</p>
+            <p className='is-size-7'>
+              Cannot load comments. Check you network.
+            </p>
+          </div>
+        </div>
+      )}
+    </section>
+  )
 })
 
 Utterances.propTypes = {
